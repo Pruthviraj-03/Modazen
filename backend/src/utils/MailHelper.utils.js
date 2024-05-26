0154db53b74a367a0066b7c765611a56ab6d8e04
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-05-26 12:08:50 +0530

    update mailHelper util file.

--- backend/src/utils/MailHelper.utils.js
+++ backend/src/utils/MailHelper.utils.js
@@ -6,31 +6,42 @@ dotenv.config({
 });
 
 const mailHelper = async (user) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 465,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
-
-  // Email content
-  const message = {
-    from: "updates@modazen.com",
-    to: user.email,
-    subject: user.subject,
-    text: user.message,
-    html: user.htmlMessage,
-  };
-
   try {
+    if (
+      !user ||
+      !user.email ||
+      !user.subject ||
+      !user.message ||
+      !user.htmlMessage
+    ) {
+      throw new Error("Incomplete user object provided for sending email");
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: process.env.SMTP_PORT == 465,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    // Email content
+    const message = {
+      from: "updates@modazen.com",
+      to: user.email,
+      subject: user.subject,
+      text: user.message,
+      html: user.htmlMessage,
+    };
+
     const info = await transporter.sendMail(message);
     console.log("Email sent:", info.response);
+    return info;
   } catch (error) {
-    console.error("Error:", error);
-    throw error;
+    console.error("Error sending email:", error.message);
+    throw new Error("Failed to send email");
   }
 };
 
