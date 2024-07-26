ce4a5bf2e42eee3fa054c6919e910fd3110fe035
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-07-26 11:58:33 +0530

    backend code cleaning

--- backend/src/models/images.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const imageSchema = new mongoose.Schema({
-  localPath: { type: String, required: true, unique: true },
-  cloudinaryUrl: { type: String, required: true },
-});
-
-const Image = mongoose.model("Image", imageSchema);
-export { Image };
